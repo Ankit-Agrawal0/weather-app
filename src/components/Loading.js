e8a3d2065003956597d193b448b0950001a409f9
Ankit-Agrawal0 <ankitagrawal160207@gmail.com>
2021-07-28 12:15:07 +0530

    made loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,10 +3,7 @@ import '../scss/Loading.scss'
 const Loading = () => {
     return (
         <div className='SECTION'>
-            <div className="center">
-                <div className="loader"></div>
-                <p className="text">Weather is loading....</p>
-            </div>
+            
         </div>
     )
 }
